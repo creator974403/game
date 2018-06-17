e9565ada84b7b113da82e8bfa76f9a9ae1e471f3
Nodir <sinigami83@mail.ru>
2018-06-17 13:12:18 +0500

    changed function new_olf_game

--- new_old_game.c
+++ new_old_game.c
@@ -4,133 +4,167 @@
 #define DELAY_DURATION 100
 #define KEY_ESCAPE 27
 
-typedef struct star{
-    int cur_x;  /* текущее положение по горизонтали */
-    int cur_y;  /* текущее положение по вертикали */
-    int max_x;  /* максимальный предел терминала по горизонтали */
-    int max_y;  /* максимальный предел терминала по вертикали */
-} Asterisk;
- 
-typedef struct {
-    int x;
-    int y;
-} point;
-
-void initial_state(Asterisk *s, int x, int y) /* начальное состояние */
-{
-    int walls[s->max_x][s->max_y];
-    int i, j;
-
-    for (i = 0; i <= s->max_x; ++i) {
-        for (j = 0; j <= s->max_y; ++j) {
-            walls[i][j] = ' ';
-        }
-    }
-
-    for (i = 0; i <= s->max_x; ++i) {
-        for (j = 0; j <= s->max_y; ++j)
-            walls[i][j] = 'x';
-    }
-
-
-    for (i = 0; i <= s->max_y; ++i) {
-        for (j = 0; j <= s->max_x; ++j) {
-            move(i, j);
-            addch(walls[j][i]);
-            refresh();
-        }
-    }
-
-    s->cur_x = x/2; 
-    s->cur_y = y-1;
-
-    move(s->cur_y, s->cur_x);
-    addch('*');
-    refresh();
-}
-
-void clean_line(Asterisk *s)
-{
-    move(s->cur_y, s->cur_x);
+/*void draw_border(Border old_border, Border new_border) {
+  for (int y = 0; y < SCREEN_HEIGHT; y++) {
+    move(y, old_border[y].left);
     addch(' ');
-    refresh();
-}
-
-void paint_move_star(Asterisk *s)
-{
-    move(s->cur_y, s->cur_x);
-    addch('*');
-    refresh();
-}
-
-void max_facet(Asterisk *s)
-{
-    if (s->cur_x >= s->max_x) {
-        s->cur_x = s->max_x - 1;
-    } else {
-        if (s->cur_x < 0) {
-            s->cur_x = 0;
-        }
-    }
-
-    if (s->cur_y >= s->max_y) {
-        s->cur_y = s->max_y - 1;
-    } else {
-        if (s->cur_y < 0) {
-            s->cur_y = 0;
-        }
-    }
-}
-
-void move_star(Asterisk *s, int x, int y)
-{
-    clean_line(s);
-    s->cur_x += x; 
-    s->cur_y += y;
-    max_facet(s);
-    paint_move_star(s);
-}
-
-int main()
-{
-    int ord_x, ord_y, key;
-    Asterisk s;
-
-    timeout(DELAY_DURATION); /* эта функция нам понадобится, 
-    когда будет делать функцию автоматической езды без ошибания наших действий */
+    move(y, new_border[y].left);
+    addch('#');
     
+    move(y, old_border[y].right);
+    addch(' ');
+    move(y, new_border[y]. right);
+    addch('#');
+  }
+}*/
+
+typedef struct {
+    int x; 
+    int y;
+} Point;
+
+typedef enum {
+    MoveLeft,
+    MoveRight, 
+    MoveUp, 
+    MoveDown
+} Move;
+
+typedef struct {
+    int min_x, max_x;
+    int min_y, max_y;
+} Border;
+
+Point get_next_car_position(Point current_position, Move move, Border borders) {
+    Point p;
+
+    switch(move) {
+    case MoveLeft: {
+        p.x = current_position.x - 1;
+        p.y = current_position.y;
+        break;
+    }
+    case MoveRight: {
+        p.x = current_position.x + 1;
+        p.y = current_position.y;
+        break;
+    }
+    case MoveUp: {
+        p.x = current_position.x;
+        p.y = current_position.y - 1;
+        break;
+    }
+    case MoveDown: {
+        p.x = current_position.x;
+        p.y = current_position.y + 1;
+        break;
+    }
+  }
+  
+  /*if (p.x + CAR_WIDTH > border.max_x) { p.x = ...; }
+  if (p.x - CAR_WIDTH < border.min_x) { p.x = ...; }
+  // ...*/
+  
+  return p;
+}
+
+void draw_car_at_point(Point current_car_position, Point new_car_position) {
+    move(current_car_position.y, current_car_position.x);
+    addch(' ');
+    move(new_car_position.y, new_car_position.x);
+    addch('*');
+}
+
+typedef enum {
+    ActionNothing,
+    ActionQuit,
+    ActionMoveLeft,
+    ActionMoveRight,
+    ActionMoveUp,
+    ActionMoveDown,
+}   Action;
+
+Action next_action() {
+    int key = getch();
+      
+    if (key == KEY_ESCAPE) {
+        return ActionQuit;
+    }
+      
+    if (key == KEY_UP) {
+        return ActionMoveUp;
+    }
+  
+    if (key == KEY_DOWN) {
+        return ActionMoveDown;
+    }
+  
+    if (key == KEY_LEFT) {
+        return ActionMoveLeft;
+    }
+  
+    if (key == KEY_RIGHT) {
+        return ActionMoveRight;
+    }
+    return ActionNothing;
+}
+
+int main() {
+    Point car_position;
+    Border border;
+    int ord_x, ord_y;
+    Action action;
+
     initscr(); /* вызвали библиотеку*/ 
     curs_set(0); /* скрыть курсив клавиатуры */
     cbreak(); /* введеный символ доступен сразу же после ввода */
     keypad(stdscr, 1); /* обработка escape поседовательности */ 
-    noecho(); /* скрыть отображение введенного символа */
-    
-    getmaxyx(stdscr, ord_y, ord_x); /* узнать высату и ширину экрана*/
-    s.max_x = ord_x;
-    s.max_y = ord_y;
-    initial_state(&s, ord_x, ord_y);
+    noecho(); 
 
-    while((key = getch())!= KEY_ESCAPE) {
-        switch(key) {
-            case KEY_UP:
-                move_star(&s, 0, -1);
-                break;
-            case KEY_DOWN:
-                move_star(&s, 0, 1);
-                break;
-            case KEY_LEFT:
-                move_star(&s, -1, 0);
-                break;
-            case KEY_RIGHT:
-                move_star(&s, 1, 0);
-                break;
-            case KEY_RESIZE: /* передает значение если размер экрана 
-                                изменился*/
-                //handle_resize(&x, &y, &max_x, &max_y);
-                break;
+    getmaxyx(stdscr, ord_y, ord_x);
+    border.min_x = 0;
+    border.max_x = ord_x;
+    border.min_y = 0;
+    border.max_y = ord_y;
 
+    car_position.x = ord_x/2;
+    car_position.y = ord_y/2;
+    move(car_position.y, car_position.x);
+    addch('*');
+    refresh();
+
+    while( (action = next_action()) != ActionQuit) {
+        Point old_position = car_position;
+        switch(action) {
+            case ActionMoveUp: {
+                car_position = get_next_car_position(car_position, MoveUp, border);
+                draw_car_at_point(old_position, car_position);
+                break;
+            }
+            case ActionMoveDown: {
+                car_position = get_next_car_position(car_position, MoveDown, border);
+                draw_car_at_point(old_position, car_position);
+                break;
+            }
+            case ActionMoveLeft: {
+                car_position = get_next_car_position(car_position, MoveLeft, border);
+                draw_car_at_point(old_position, car_position);
+                break;
+            }
+            case ActionMoveRight: {
+                car_position = get_next_car_position(car_position, MoveRight, border);
+                draw_car_at_point(old_position, car_position);
+                break;
+            }
+            default:
+                break;
         }
-    }
-    endwin();
-    return 0;
+    /*Border old_border = border;
+    border = get_next_border(border ... );
+    draw_border(old_boder, border);*/
+    refresh();
+  }
+
+   endwin();
+   return 0;
 }
