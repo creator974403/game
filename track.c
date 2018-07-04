2d35973ad59e3db6d4f40bc366a8d0073c126dfd
Nodir <sinigami83@mail.ru>
2018-07-04 21:43:09 +0500

    again fixed small bags in new function

--- track.c
+++ track.c
@@ -47,7 +47,8 @@ void draw_edges_of_track(int *fence, int ord_y)
     fence[0] = temp;
 }
 
-void draw_track_at_screen_offset(Track track, Screen screen, int screen_y_offset) {
+void draw_track_at_screen_offset(Track track, Screen screen, int screen_y_offset) 
+{
     TrackFence *fences = track.fences;
     int fences_count = track.fences_count;
     int i;
@@ -57,7 +58,7 @@ void draw_track_at_screen_offset(Track track, Screen screen, int screen_y_offset
         if (f.y_offset > screen_y_offset + screen.ord_y) {
             break;
         }
-        if (f.y_offset + f.lenght < screen_y_offset) {
+        if (f.y_offset + f.length < screen_y_offset) {
             continue;
         }
         if (f.y_offset < screen_y_offset) {
@@ -78,18 +79,18 @@ void draw_fence(int y_offset,
   int left = (screen.ord_x - track.width) / 2;
   int right = (screen.ord_x + track.width) / 2;
   int y;
-  for (y = y_offset; y < length; ++i) {
+  for (y = y_offset; y < length; ++y) {
     if (side == TrackFenceSideLeft) {
         int x;
         for (x = 0; x < width; ++x) {
             move(y, left + x);
-            addch("#");
+            addch('#');
         }
     } else {
         int x;
         for (x = 0; x < width; ++x) {
             move(y, right - x);
-            addch("#");
+            addch('#');
       }
     }
   }
